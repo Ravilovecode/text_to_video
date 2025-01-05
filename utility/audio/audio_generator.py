1df87b6c80fe7c59cb60727c8fd4f85039b319ca
sunny84patel <patel844732sunny@gmail.com>
2025-01-05 20:34:03 +0530

    new changes

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,7 +1,8 @@
 import edge_tts
 
 async def generate_audio(text,outputFilename):
-    communicate = edge_tts.Communicate(text,"en-AU-WilliamNeural")
+    # communicate = edge_tts.Communicate(text,"en-AU-WilliamNeural")
+    communicate = edge_tts.Communicate(text,"hi-IN-SwaraNeural")
     await communicate.save(outputFilename)
 
 
